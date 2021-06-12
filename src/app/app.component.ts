02683c927bd0b3a03ea74b1ff03fec826d8ac8b7
Lonnie Mitchell <lonniejmitchell@gmail.com>
2021-06-12 16:31:37 -0500

    edited

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Mr. Bigglesworth';
+  city = 'San Antonio';
+  tagline = 'Hope for the Best but Always Prepare for the Worst!';
+  aboutMe = 'I am as former US Navy Nuclear Electronics Technician who became a Rotating Equipment Specialist once I got out for the Oil and Gas Industry.  I will be finishing school for Fullstack Development at the end of the year and look forward to the challenges in my new career.'
+}
